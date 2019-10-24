bbd5166d44dc10014d0edc3264ad7b3725590e72
Syam Ahmad <syammehar@hotmail.com>
2019-10-24 21:15:20 +0500

    changes

--- components/variable.js
+++
@@ -1,5 +0,0 @@
-const appIp = "39.50.110.108";
-const appPort = "8090";
-export const ApiUrl = "http://" + appIp + ":" + appPort + "/api/";
-export const TokenUrl = "http://" + appIp + ":" + appPort + "/token";
-export const BaseUrl = "http://" + appIp + ":" + appPort + "/";
